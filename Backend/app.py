407fa6c8fc0739778743759f1a14e1b3871d0b47
silvia730 <silvianjeri730@gmail.com>
2025-07-12 21:59:34 +0300

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -261,4 +261,7 @@ def delete_user(user_id):
 if __name__ == '__main__':
     initialize_db()
     populate_sample_data()
-    app.run(debug=True)
+    
+    port = int(os.environ.get('PORT', 5000))  # Use PORT from env or default to 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
+
